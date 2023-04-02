492c4c93955d17ffb12ce7d29149a1fa4fad5921
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-04-02 02:31:59 -0400

    update calculation to show correct countdown
    Change Hours Calculation: const hours from ((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); to
     = Math.floor(timeRemaining / (1000 * 60 * 60)); Real talk this is the work of ChatGPT. In the first example
     upon closer inspection the time was all wrong. With prompting we were able to get the formula correct to calculate
     the countdown correctly. If anyone can come up with a more elegant solution I would love to see it and learn
    
     Change Seconds to display seconds properly  ("hours").innerHTML = ("000" + hours).slice(-3); change to 3 placeholders instead of 2

--- src/js/app.js
+++ src/js/app.js
@@ -19,34 +19,21 @@ let x = setInterval(function() {
   const timeRemaining = countDownDate - now;
 
   // Calculate the hours, minutes, and seconds remaining
-  const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const hours = Math.floor(timeRemaining / (1000 * 60 * 60));
   const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
   // Display the time remaining in the timer
-  document.getElementById("hours").innerHTML = ("0" + hours).slice(-2);
+  document.getElementById("hours").innerHTML = ("000" + hours).slice(-3);
   document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
   document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
 
   // If the countdown is over, stop updating the timer and remove the countDownDate from local storage
   if (timeRemaining < 0) {
     clearInterval(x);
-    document.getElementById("hours").innerHTML = "00";
+    document.getElementById("hours").innerHTML = "000";
     document.getElementById("minutes").innerHTML = "00";
     document.getElementById("seconds").innerHTML = "00";
     localStorage.removeItem("countDownDate");
   }
 }, 1000);
-
-// Summary:
-// This code creates a countdown timer that counts down to the next Saturday at 11:59:59 PM. The code checks if the countDownDate value is already stored in local storage, and if not, it calculates the date and time of the next Saturday and sets it as the value of countDownDate. The code updates the countdown timer every second and displays the time remaining in hours, minutes, and seconds. When the countdown is over, the code stops updating the timer and removes the countDownDate value from local storage.
-
-// Explanation:
-// The code first checks if the countDownDate value is already stored in local storage using the localStorage.getItem() method. If the value is not stored, the code calculates the date and time of the next Saturday using the Date() constructor and sets it as the value of countDownDate. The code then stores the countDownDate value in local storage using the localStorage.setItem() method.
-
-// The code then sets up an interval using the setInterval() method that updates the countdown timer every second. Inside the interval function, the code gets the current date and time using the Date().getTime() method and calculates the time remaining until the countDownDate using simple arithmetic operations.
-
-// The code then calculates the hours, minutes, and seconds remaining using the Math.floor() and modulus (%) operators, and displays the time remaining in the timer by setting the innerHTML property of the corresponding HTML elements using the document.getElementById() method.
-
-// Finally, if the countdown is over (i.e., the timeRemaining value is less
-// ChatGPT Mar 14 Version
